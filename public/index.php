82a5803224a17ec1b0ea8e5e7a724b3980b5b8ac
mirahs <mirahs@163.com>
2019-01-28 17:08:37 +0800

    修改

--- public/index.php
+++ public/index.php
@@ -1,11 +1,13 @@
 <?php
+// public目录
+define('APP_PUBLIC',  __DIR__ . '/');
 // 应用目录
 define('APP_PATH',  __DIR__ . '/../app/');
 // 是否调试
 define('APP_DEBUG', true);
 
 // 默认应用
-define('DEFAULT_APP',           'home');
+define('DEFAULT_APP',           'cp');
 // 默认控制器
 define('DEFAULT_CONTROLLER',    'Index');
 // 默认方法
